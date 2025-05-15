d14439a05cc74d304ea2706871a4a6921203d933
Clement <clement@LPTP005.exaprint.lan>
2025-05-15 23:52:32 +0200

    Workflow test again

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -18,11 +18,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
     
   });
-
-  it('should not have a Test value rendered', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.test p').textContent).not.toContain('Test');
-  });
 });
